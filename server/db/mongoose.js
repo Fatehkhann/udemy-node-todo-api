ae7bae3c77842060f58e0c83367657f331279649
fateh khan <fateh khan>
2017-03-11 02:37:17 +0500

    TODO APP FINISHED

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://Fateh:bravoXc234@ds127730.mlab.com:27730/todos');
-
+mongoose.connect(process.env.MONGODB_URI);
 module.exports = {mongoose};
