166220a6b185819d3f1b3b5cfe09fe80e64dfd5e
fateh khan <fateh khan>
2017-03-12 16:57:20 +0500

    Middleware Ready

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -28,19 +29,19 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (e) => {
         res.status(400).send(e);
     });
 });
 
 app.get('/todos/:id', (req, res) => {
-    if(!ObjectID.isValid(req.params.id)) {
+    if (!ObjectID.isValid(req.params.id)) {
         console.log('Not a valid ID');
     }
     else {
         Todo.findById(req.params.id).then((todo) => {
-        res.send({todo});
+            res.send({ todo });
         }, (e) => {
             res.status(400).send(e);
         });
@@ -49,14 +50,14 @@ app.get('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res) => {
     Todo.findByIdAndRemove(req.params.id).then((doc) => {
-        if(!ObjectID.isValid(req.params.id)) {
-        console.log('Not a valid ID');
-    }
-        if(!doc) {
+        if (!ObjectID.isValid(req.params.id)) {
+            console.log('Not a valid ID');
+        }
+        if (!doc) {
             res.status(404).send();
         }
         res.send(doc)
-    }).catch( (e) => {
+    }).catch((e) => {
         res.status(400).send(e);
     });
 });
@@ -65,27 +66,30 @@ app.patch('/todos/:id', (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']);
 
-    if(!ObjectID.isValid(req.params.id)) {
+    if (!ObjectID.isValid(req.params.id)) {
         console.log('Not a valid ID');
     }
 
-    if(_.isBoolean(body.completed) && body.completed)
-    {
+    if (_.isBoolean(body.completed) && body.completed) {
         body.completedAt = new Date().getTime();
     } else {
         body.completed = false;
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) =>{
-        if(!todo) {
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
             res.status(400).send();
         }
 
-        res.send({todo});
+        res.send({ todo });
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.post('/users', (req, res) => {
@@ -94,8 +98,8 @@ app.post('/users', (req, res) => {
 
     user.save().then(() => {
         return user.generateAuthToken();
-    }).then ((token) => {
-        res.header('x-auth', token).send(user)
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
     });
@@ -105,4 +109,4 @@ app.listen(port, () => {
     console.log('Started on port ', port);
 });
 
-module.exports = {app};
+module.exports = { app };
